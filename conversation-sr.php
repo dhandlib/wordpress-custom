2bb06e528d0b274738de470c8f3fdcd9584ff707
Roxanne Shirazi <1751214+rshirazi@users.noreply.github.com>
2018-03-07 20:39:49 -0500

    Add files via upload

---
+++ conversation-sr.php
@@ -0,0 +1,52 @@
+<?php get_header(); ?>
+
+<div id="main" class="row">
+  	
+
+	<div id="content" class="col9">
+		
+		<div id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?>>
+          	<div class="sr-header">
+            </div>  
+         <h1>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
+				<?php bfa_comments_number(); ?>
+			</h1> 
+			<div class="post-footer">
+              A conversation between <?php echo do_shortcode('[post_authors]'); ?>
+<br/>
+				<?php the_time( 'j M Y' ); ?>&nbsp;&nbsp;| <?php the_tags( '<p class="post-tags">', ' &middot; ', '</p>' ); ?>
+			</div>
+         
+
+			<div class="post-bodycopy cf">
+				<?php the_content(); ?>
+				<?php wp_link_pages( array( 
+					'before' => __( '<p class="post-pagination">Pages:', 'montezuma' ), 
+					'after' => '</p>'
+				) ); ?>
+			</div>
+ 
+          
+			<?php edit_post_link( __( "Edit", 'montezuma' ) ); ?>
+			
+
+
+		</div>
+
+		<?php comments_template( '', true ); ?>
+
+		<nav class="singlenav cf">
+			<div class="older"><?php previous_post_link( '&laquo; %link', __('Previous', 'montezuma') ); ?></div>
+			<div class="newer"><?php next_post_link( '%link &raquo;', __('Next', 'montezuma') ); ?></div>
+		</nav>
+		
+	</div>
+  
+<div id="widgetarea-SR" class="col3 widgetarea-sr">
+		<?php dynamic_sidebar( 'Widget Area SR' ); ?>
+	</div>	
+	
+</div>
+
+<?php get_footer(); ?>
---
+++ special-issue.php
@@ -0,0 +1,61 @@
+<?php get_header(); ?>
+
+<div id="main" class="row">
+  	
+
+	<div id="content" class="col9">
+		
+		<nav class="singlenav cf">
+			<div class="older"><?php previous_post_link( '&laquo; %link', __('Previous', 'montezuma') ); ?></div>
+			<div class="newer"><?php next_post_link( '%link &raquo;', __('Next', 'montezuma') ); ?></div>
+		</nav>
+		
+		<div id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?>>
+
+			<h1>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
+				<?php bfa_comments_number(); ?>
+			</h1>
+			
+			<div class="post-footer">
+				Edited by Caitlin Christian-Lamb, Zach Coble, Thomas Padilla, Caro Pinto, Sarah Potvin, John Russell, Roxanne Shirazi, and Patrick Williams
+              </div>
+          
+          <div class="post-footer">
+				<?php the_time( 'j M Y' ); ?>
+			</div>
+          <div id="widgetarea-SI16" class="widgetarea-si">
+		<?php dynamic_sidebar( 'Widget Area SI16' ); ?>
+	</div>	
+          <div id="sharing-widget">
+            <?php dynamic_sidebar( 'Sharing widget area' ); ?>
+          </div>
+         
+			<div class="post-bodycopy cf">
+				<?php the_content(); ?>
+				<?php wp_link_pages( array( 
+					'before' => __( '<p class="post-pagination">Pages:', 'montezuma' ), 
+					'after' => '</p>'
+				) ); ?>
+			</div>
+ 
+          
+			<?php edit_post_link( __( "Edit", 'montezuma' ) ); ?>
+			
+
+
+		</div>
+
+		<?php comments_template( '', true ); ?>
+
+		<nav class="singlenav cf">
+			<div class="older"><?php previous_post_link( '&laquo; %link', __('Previous', 'montezuma') ); ?></div>
+			<div class="newer"><?php next_post_link( '%link &raquo;', __('Next', 'montezuma') ); ?></div>
+		</nav>
+		
+	</div>
+	
+	
+</div>
+
+<?php get_footer(); ?>
